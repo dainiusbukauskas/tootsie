c222fc2dde7bae4aabb2238632758427c5c31126
dainiusbukauskas <dainius0gb@gmail.com>
2018-01-01 13:45:18 -0500

    add cleanUp and frameAdjustment functionality

--- imagebatchtest_pillow_01.py
+++ imagebatchtest_pillow_01.py
@@ -8,13 +8,29 @@ import glob, os, shutil, csv
 
 
 
+def cleanUpVideoNames(dir, pattern, titlePattern):
+    for pathAndFilename in glob.iglob(os.path.join(dir,pattern)):
+        title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+
+        index = title[:3]
+        frameNumber = title[-3:]
+        tag = title[3:-3]
+
+        os.rename(pathAndFilename,
+                  os.path.join(dir,titlePattern % index, tag, frameNumber)
+
 
 def rename(dir, pattern, titlePattern):
     for pathAndFilename in glob.iglob(os.path.join(dir,pattern)):
         title, ext = os.path.splitext(os.path.basename(pathAndFilename))
-        os.rename(pathAndFilename,
-                  os.path.join(dir,titlePattern % title + ext))
+##        os.rename(pathAndFilename,
+##                  os.path.join(dir,titlePattern % title + ext))
 
+        # print(pathAndFilename)
+        #print(os.path.basename(pathAndFilename))
+        head, tail = os.path.split(pathAndFilename)
+        print('head:' + head)
+        print('tail:' + tail)
 
 def removeOver180Frames(dir, pattern):
     for pathAndFilename in glob.iglob(os.path.join(dir,pattern)):
@@ -27,7 +43,7 @@ def removeOver180Frames(dir, pattern):
         if lastThreeCharsInt > 180:
             shutil.move(title+ext,'over180FramesFolder/' + title + ext)
 
-def identifyTimeAdjustedVideos(dir, pattern, timeAdjustmentData):
+def identifyAndAdjustTimeAdjustedVideos(dir, pattern, timeAdjustmentData):
     tag = timeAdjustmentData[0]
     startFrame = timeAdjustmentData[3]
     endFrame = timeAdjustmentData[4]
@@ -37,42 +53,47 @@ def identifyTimeAdjustedVideos(dir, pattern, timeAdjustmentData):
         title, ext = os.path.splitext(os.path.basename(pathAndFilename))
         if tag in title:
             print('found a match: ' + title)
+            incrementFrameCounterForAGivenImage(pathAndFilename, startFrame)
 
 
 def incrementFrameCounterForAGivenImage(pathAndFilename,counterIncrement):
     title, ext = os.path.splitext(os.path.basename(pathAndFilename))
     lastThreeChars = title[-3:]
+    beforeLastThreeChars = title[:-3]
     # print(lastThreeChars)
     lastThreeCharsInt = int(lastThreeChars)
-    lastThreeCharsInt += counterIncrement
+    lastThreeCharsInt += int(counterIncrement)
 
     # rename so that the last three characters of the title are replaced with the newly incremented frame numbers.
     
-    # os.rename(pathAndFilename,
-    #          os.path.dirname(pathAndFilename) + )
+    os.rename(pathAndFilename,
+              os.path.dirname(pathAndFilename) + '/' + beforeLastThreeChars + str(lastThreeCharsInt) + ext)
+
                              
-            
+def incrementFrameCounterForAllVideosIdentifiedInTheCSVFile(csvFilePath,imageDirectory):
+    with open (csvFilePath, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter = ',', quotechar = '|')
+        counter = 0
+        for row in reader:
+            # print(row[0])
+            if counter != 0:
+                # tags.append(row[0])
+                # tag = row[0]
+                identifyAndAdjustTimeAdjustedVideos(imageDirectory,r'*.jpg',row)
+                # if row[0] 
+            counter = counter + 1
 
-
-# rename(r'./',r'*.jpg',r'new(%s)')
-
-# removeOver180Frames(r'./', r'*.jpg')
-
-with open ('frameTimingAdjustments.csv', 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter = ',', quotechar = '|')
-    counter = 0
-    for row in reader:
-        # print(row[0])
-        if counter != 0:
-            # tags.append(row[0])
-            # tag = row[0]
-            identifyTimeAdjustedVideos(r'./img',r'*.jpg',row)
-            # if row[0] 
-        counter = counter + 1
-
-    print('finished') 
+        print('finished') 
 
     # del tags[0]
     # print(tags)
         
     
+            
+
+
+# rename(r'./img/',r'*.jpg',r'new(%s)')
+
+# removeOver180Frames(r'./', r'*.jpg')
+
+incrementFrameCounterForAllVideosIdentifiedInTheCSVFile('frameTimingAdjustments.csv',r'./test/')
